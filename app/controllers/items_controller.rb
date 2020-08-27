20c289aaf5e38e65f4c7dfe7f07fd79c116f29e2
fumn112026 <fumn112026@gmail.com>
2020-08-28 01:09:28 +0900

    MVCを編集
    トップページの出品ボタンのリンクを設定。出品ページへのルーティング、コントローラー、マイグレーションファイルを記述。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :barnd, :condition, :status, :shipping_cost, :shipping_date, :price)
+  end
 end
